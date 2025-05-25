f3ca47b26bc76d7a02550b98b9be48e9b9f78ba8
heitordeve <heitor.developer@outlook.com>
2025-05-25 15:33:02 -0300

    ► DEV ◄ Aprendendo sobre Swtich e trabalhando conversões de tipos

---
+++ Clean Architecture/RevisãoCSharpBasico/2. Condicionais (if, else, switch)/Desafio2.2-Switch/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+
+Console.WriteLine(" === Desafio 2.2 - Switch === \n");
+Console.WriteLine(" Peça para informar a nota do aluno.  \n");
+Console.WriteLine(" Em seguida verifique se está aprovado, reprovado ou em recuperação \n");
+Console.WriteLine(" Mamão?! Bora lá! \n\n\n");
+
+Console.Write(" Digite o nome do aluno: ");
+string? entradaNome = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(entradaNome))
+{
+    Console.WriteLine("❌ Nome inválido. Encerrando o programa.");
+    return;
+}
+string nome = entradaNome!;
+
+Console.Write(" Digite a nota do aluno: ");
+string? entradaNota = Console.ReadLine();
+
+if (!double.TryParse(entradaNota, out double nota))
+{
+    Console.WriteLine("❌ Nota inválida. Encerrando o programa.");
+    return;
+}
+
+switch (nota)
+{
+    case > 6:
+        Console.WriteLine($"✅ A nota do aluno(a) {nome} é {nota}, portanto está aprovado.");
+        break;
+    case 6:
+        Console.WriteLine($"⚠️ A nota do aluno(a) {nome} é {nota}, portanto está em recuperação.");
+        break;
+
+    case < 6:
+        Console.WriteLine($"❌ A nota do aluno(a) {nome} é {nota}, portanto está reprovado.");
+        break;
+    default:
+        Console.WriteLine("fechou");
+        break;
+}
+
+Console.WriteLine("\nPressione qualquer tecla para sair...");
+Console.ReadKey();
