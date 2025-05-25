d6a2a3a5914dd8e37ca425d9d00dd9c3ad737e2b
heitordeve <heitor.developer@outlook.com>
2025-05-25 15:05:04 -0300

    ► DEV ◄ Aprendendo sobre estrutura simples e composta com IF ELSE

---
+++ Clean Architecture/RevisãoCSharpBasico/2. Condicionais (if, else, switch)/Desafio2.1-IfElse/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+Console.WriteLine(" === Desafio 2.1 === \n");
+Console.WriteLine(" Peça para informar a nota do aluno.  \n");
+Console.WriteLine(" Em seguida verifique se está aprovado, reprovado ou em recuperação \n");
+Console.WriteLine(" Mamão?! Bora lá! \n\n\n");
+
+string nome;
+double nota = 0;
+
+Console.Write(" Digite o nome do aluno: ");
+nome = Console.ReadLine();
+
+Console.Write(" Digite a nota do aluno: ");
+nota = double.Parse(Console.ReadLine());
+
+
+if (nota > 6)
+{
+    Console.WriteLine($"✅ A nota do aluno(a) {nome} é {nota}, portanto está aprovado.");
+}
+else if (nota == 6)
+{
+    Console.WriteLine($"⚠️ A nota do aluno(a) {nome} é {nota}, portanto está em recuperação.");
+}
+else
+{
+    Console.WriteLine($"❌ A nota do aluno(a) {nome} é {nota}, portanto está reprovado.");
+}
+
+
+Console.WriteLine("\nPressione qualquer tecla para sair...");
+Console.ReadKey();
