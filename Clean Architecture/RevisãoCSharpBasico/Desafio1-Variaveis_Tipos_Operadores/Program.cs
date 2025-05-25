27ea8b75424f8143a753d6ad51d188bf1f5ca116
heitordeve <heitor.developer@outlook.com>
2025-05-25 14:22:44 -0300

    ► Dev ◄ Desafio 1: Crie um programa que receba nome, idade e salário de um usuário e imprima os dados formatados.

---
+++ Clean Architecture/RevisãoCSharpBasico/Desafio1-Variaveis_Tipos_Operadores/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(" === Desafio 1 === \n");
+Console.WriteLine(" Crie um programa em que o usuário posso digitar no console:  \n");
+Console.WriteLine(" nome, idade e salário de um usuário. \n");
+Console.WriteLine(" Em seguida fazer a impressão no console. \n");
+Console.WriteLine(" Mamão?! Bora lá! \n\n\n");
+
+
+Console.Write(" Digite o nome: ");
+string nome = Console.ReadLine();
+
+Console.Write(" Digite a sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+
+Console.Write(" Digite o seu salário: ");
+double salario = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Olá, " + nome + "\n");
+Console.WriteLine("Vocé tem " + idade + " anos." + "\n");
+Console.WriteLine("O seu salário é R$ " + salario + "\n");
